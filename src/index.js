6437173c5e944a8d64a9823f0d7cb38177da3839
spiraljacobs <spiraljacobs@gmail.com>
2019-07-11 13:12:40 +0100

    Checking in changes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox Test</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Great State Backend Team Rules</h1>
+      <h2>cool cool cool cool</h2>
     </div>
   );
 }
